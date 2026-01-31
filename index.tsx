c4f228f2d6fed214697d295635f73914e3269938
sparrowsolutionsuy <sparrowsolutionsuy@gmail.com>
2026-01-31 20:23:59 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("No se pudo encontrar el elemento root");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
