54b8189db7798a5ff40fea7c9b64363d216577e9
sparrowsolutionsuy <sparrowsolutionsuy@gmail.com>
2026-01-31 20:37:34 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import App from './src/App'; // <--- Importante: apunta a la carpeta src
+import App from './src/App'; 
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("No se pudo encontrar el elemento root para montar la aplicación");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
-);
