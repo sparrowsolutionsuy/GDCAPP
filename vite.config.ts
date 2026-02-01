17c0fac0991a6f261ba7812c7cab1eb43dbe6c7a
sparrowsolutionsuy <sparrowsolutionsuy@gmail.com>
2026-01-31 21:40:23 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GDCAPP/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  // Asegúrate de que esto coincida con el nombre de tu repositorio en GitHub
+  base: '/GDCAPP/', 
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
+  }
 });
